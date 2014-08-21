430981c1e9d7e39d4a9fc0afcac8dfd0ae609ae3
philhofer <phofer@umich.edu>
2014-08-21 00:16:18 -0400

    fix typo

--- chng.go
+++ chng.go
@@ -2,17 +2,15 @@
 
 chng := func(o rkive.Object) error {
 	v := o.(*rkive.Blob)
-	
 	// the change already happened
 	if bytes.Equal(v.Data, []byte("New Data!") {
 		return rkive.ErrDone
 	}
-
 	// the change hasn't happened yet
 	if bytes.Equal(v.Data, old) {
 		v.Data = []byte("New Data!")
+		return nil
 	}
-	
 	// the value was changed in the database;
 	// re-evaluate
 	return rkive.ErrModified
